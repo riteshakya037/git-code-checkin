96020f78ccb591f3cc83be70a718bfa5c77f66e5
rishakya <rishakya@deeerwalk.com>
2016-02-26 17:17:33 +0545

    Changes implemented

--- git_code_checkin.py
+++ git_code_checkin.py
@@ -15,6 +15,16 @@ changed_files_dict["New"] = list()
 changed_files_dict["Modified"] = list()
 changed_files_dict["Renamed"] = list()
 changed_files_dict["Deleted"] = list()
+# Console colorse
+W = '\033[0m'  # white (normal)
+R = '\033[31m'  # red
+G = '\033[32m'  # green
+O = '\033[33m'  # orange
+B = '\033[34m'  # blue
+P = '\033[35m'  # purple
+C = '\033[36m'  # cyan
+GR = '\033[37m'  # gray
+
 
 def main():
     # Parse arguments
@@ -26,9 +36,8 @@ def main():
 
     # Git commands to get all the required information
     list_changed_files_cmd = "git diff-tree --no-commit-id --name-status -r -M {}".format(hash)
-    commit_in_branch_cmd = "git branch -r --contains {}".format(hash)
-    commit_hash_and_message_cmd = "git log --pretty=oneline | grep {}".format(hash)
 
+    # Get list of all files
     pipe = subprocess.Popen(list_changed_files_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     out, err = pipe.communicate()
 
@@ -47,17 +56,8 @@ def main():
         else:
             pass
 
-    # Get additional inputs
-    try:
-        project = input("Project: ") or "N/A"
-        reviewed_by = input("Code/Unit Test Reviewed By: ") or "N/A"
-        qat_by = input("QAT By: ") or "N/A"
-        summary = input("Summary: ") or "N/A"
-        impacts = input("Impacts: ") or "N/A"
-        notes = input("Notes: ") or "N/A"
-    except KeyboardInterrupt:
-        sys.exit()
-
+    # get name of branch
+    commit_in_branch_cmd = "git branch -r --contains {}".format(hash)
     pipe = subprocess.Popen(commit_in_branch_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     out, err = pipe.communicate()
 
@@ -66,77 +66,115 @@ def main():
     branch_list = [branch.strip() for branch in branch_list]
     branches = ', '.join(branch_list)
 
+    # Get full commit hash and commit message
+    commit_hash_and_message_cmd = "git log {}".format(branch_list[0]) + " --pretty=oneline | grep {}".format(hash)
+
+    # Get additional inputs
+    try:
+        while True:
+            account = input(C + "Account: " + W)
+            if account == "":
+                print(R + "Cannot leave blank Mandatory Value" + W)
+                continue
+            else:
+                break
+        while True:
+            client = input(C + "Client: " + W)
+            if client == "":
+                print(R + "Cannot leave blank Mandatory Value" + W)
+                continue
+            else:
+                break
+        while True:
+            reviewed_by = input(C + "Code/Unit Test Reviewed By: " + W)
+            if reviewed_by == "":
+                print(R + "Cannot leave blank Mandatory Value" + W)
+                continue
+            else:
+                break
+    except KeyboardInterrupt:
+        print(R + "Exiting." + W)
+        sys.exit(0)
+
     # Write an HTML file in user's 'Desktop'
-    with open("{}/Desktop/checkin.html".format(os.getenv("HOME")), 'w') as out_file:
+    with open("{}/checkin.html".format(os.getenv("HOME")), 'w') as out_file:
         pipe = subprocess.Popen(commit_hash_and_message_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         out, err = pipe.communicate()
 
         commit = str(out, encoding="utf_8").split(' ', 1)
-        commit_hash, commit_message = tuple(commit)
-
-        # Header
-        out_file.write("<div><div style=\"color: rgb(34, 34, 34);\"><h2 style=\"font-family: 'Lucida Grande', verdana, arial, helvetica, sans-serif; padding: 12px 20px; border-bottom-width: 1px; border-bottom-style: solid; border-bottom-color: rgb(174, 204, 215); color: rgb(68, 68, 68); border-top-width: 1px; border-top-style: solid; border-top-color: white; border-top-left-radius: 8px; border-top-right-radius: 8px; letter-spacing: -1px; background-image: url(https://ci5.googleusercontent.com/proxy/uePTVeN_JgVYsynEuyhDhbtB0mD6pTiFux95naGYnCiLzO_SBt6a9vE8hEb0LMrlNECS1JSVZeGt9mZ5ERQSUV8Br9Z9sOisA5P1AQ0hHkT3ze3ZPNEfnyjoZKFuRuEy-21EDuAPoa8=s0-d-e1-ft#https://deermine.deerwalk.com/themes/deerwalk-blue/images/smooth-gradient-blue.jpg); background-color: rgb(234, 242, 245);\">{}</h2></div></div>".format(commit_message.strip('\n')))
+        try:
+            commit_hash, commit_message = tuple(commit)
+        except ValueError:
+            print(R + "Cannot find Commit" + W)
+            sys.exit(0)
+        commit_messages = commit_message.split(" -")
+        i = 0;
+        # Commit Hash
+        out_file.write(
+            "<div>DasScrub Code CheckIn (" + account.title() + " " + client.title() + ")</div>")
         out_file.write("<div><br></div>")
+        # Deermine
+        if "#" in commit_messages[0]:
+            i = 1
+            deermine_list = commit_messages[0].split(",")
+            out_file.write(
+                "<div><strong>Deermines:</strong>&nbsp;</div><ul>")
+            for dm in deermine_list:
+                out_file.write(
+                    "<li><a href=\"https://svn.deerwalk.com/issues/" + dm.replace("#", "") + "\" target=\"_blank\">" +
+                    dm.replace("#", "") + "</a></li>")
+            out_file.write("</ul><br/>")
 
-        # Project
-        out_file.write("<div><b>Project:</b>&nbsp;{}</div>".format(project))
-        out_file.write("<div><br></div>")
-
-        # Task
-        out_file.write("<div><b>Task:</b>&nbsp;{}</div>".format(commit_message.split(':')[0].strip('\n')))
-        out_file.write("<div><br></div>")
-
+        # Description
+        if len(commit_messages) > 1:
+            out_file.write(
+                "<div><strong>Description:</strong>&nbsp;</div><ol>")
+            while i < len(commit_messages):
+                out_file.write(
+                    "<li>" + commit_messages[i] + "</li>")
+                i += 1
+            out_file.write("</ol></br>")
         # Code/Unit Test Reviewed By
-        out_file.write("<div><b>Code/Unit Test Reviewed By:</b>&nbsp;{}</div>".format(reviewed_by))
-        out_file.write("<div><br></div>")
-
-        # QAT By
-        out_file.write("<div><b>QAT By:</b>&nbsp;{}</div>".format(qat_by))
-        out_file.write("<div><br></div>")
-
-        # Summary
-        out_file.write("<div><b>Summary:</b>&nbsp;{}</div>".format(summary))
-        out_file.write("<div><br></div>")
-
-        # Impacts
-        out_file.write("<div><b>Impacts:</b>&nbsp;{}</div>".format(impacts))
-        out_file.write("<div><br></div>")
-
-        # Notes
-        out_file.write("<div><b>Notes:</b>&nbsp;{}</div>".format(notes))
+        out_file.write(
+            "<div><b>Code/Unit Test Reviewed By:</b>&nbsp;{}</div>".format(
+                reviewed_by))
         out_file.write("<div><br></div>")
 
         # Git Branch
-        out_file.write("<div><b>Git Branch:</b>&nbsp;{}</div>".format(branches))
+        out_file.write(
+            "<div><b>Git Branch:</b>&nbsp;{}</div>".format(branches))
         out_file.write("<div><br></div>")
 
         # Commit Hash
-        out_file.write("<div><b>Commit Hash:</b>&nbsp;{}</div>".format(commit_hash))
+        out_file.write("<div><b>Commit Hash:</b>&nbsp;{}</div>".format(
+            commit_hash))
         out_file.write("<div><br></div>")
 
         for header, file_list in changed_files_dict.items():
             if len(file_list) != 0:
-                out_file.write("<div><b>{} Files:</b></div>".format(header))
-
+                out_file.write(
+                    "<div><b>{} Files:</b></div><ul>".format(
+                        header))
                 for file in file_list:
                     i = 0
-                    out_file.write("<div>")
+                    out_file.write("<li>")
                     for token in file.split('\t'):
                         if i == 0:
-                            out_file.write("#&nbsp;&nbsp;")
+                            out_file.write("")
                         elif i == 2:
                             out_file.write(" -> " + token)
                         else:
                             out_file.write(token)
 
                         i += 1
-                    out_file.write("</div>")
+                    out_file.write("</li>")
 
-                out_file.write("<div><br></div>")
+                out_file.write("</ul><div><br></div>")
 
         out_file.write("<div>Regards,</div>")
 
     # Copy the generated HTML to clipboard
-    os.system("xclip -selection clipboard -t text/html {}/Desktop/checkin.html".format(os.getenv("HOME")))
+    os.system("xclip -selection clipboard -t text/html {}/checkin.html".format(os.getenv("HOME")))
+
 
 if __name__ == "__main__": main()
